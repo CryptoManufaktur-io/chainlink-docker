e3b27251cf9ffdcc98029a33be3fd1cbc4300c3e
Yorick Downe <yorickdowne@gmail.com>
2021-03-25 04:14:23 -0400

    Re-enable PGSQL wait script

--- chainlink/wait-for-pgsql.sh
+++ chainlink/wait-for-pgsql.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-host=$(echo $1 | awk -F/ '{print $3}' | awk -F@ '{print $2}')
+host=$(echo $1 | awk -F/ '{print $3}' | awk -F@ '{print $(NF)}')
 target="tcp://$host"
 shift
 dockerize -wait $target -timeout 60s "$@"
